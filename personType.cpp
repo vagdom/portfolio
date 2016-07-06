b0f2158bfaaf0bca67202ab9d69083f800dfd516
vagdom <vagdom@hotmail.com>
2016-07-06 13:03:47 -0400

    Update personType.cpp

--- personType.cpp
+++ personType.cpp
@@ -4,4 +4,23 @@
 void personType::print() const
 {
     cout << firstName << " " << lastName;
-}    
+} 
+
+void personType::setName(string first, string last)
+{
+    firstName = first;
+    lastName = last;
+}
+
+void personType::getName(string& first, string& last)
+{
+    first = firstName;
+    last = lastName;
+}
+
+//Constructor
+personType::personType(string first, string last)
+{
+    firstName = first;
+    lastname = last;
+}
