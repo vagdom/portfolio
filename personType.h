0b8e1bce379a40ccd38118dec967a948b2512ff3
vagdom <vagdom@hotmail.com>
2016-07-06 12:56:48 -0400

    Update personType.h

--- personType.h
+++ personType.h
@@ -12,5 +12,22 @@ public:
   //to the parameters.
   //Postcondition: first = firstName; last = lastName
   
+  void getName(string& first, string& last);
+  //Function to return firsttName and lastName via
+  //the parameters.
+  //Postcondition: first = firstname; last = lastName
+  
+  personType(string first = "", string last = "");
+  //Constructor
+  //Sets firstName and lastName according to the
+  //parameters.
+  //The default values of the parameters are empty
+  //strings.
+  //Postcondition: firstName = first; lastname = last
+  
+private:
+  string firstName;       //stores the first name
+  string lastName;        //stores the last name
+  
   
 };
