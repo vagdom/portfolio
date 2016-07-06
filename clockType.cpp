362461bb8458bdb35bd498e85c95883df5f65f9a
vagdom <vagdom@hotmail.com>
2016-07-06 11:21:09 -0400

    Update clockType.cpp

--- clockType.cpp
+++ clockType.cpp
@@ -76,3 +76,27 @@ bool clockType::equalType(const clockType& otherClock) const
            && sec == otherClock.sec);
 }
 
+clockType::clockType(int hours, int minutes, int seconds)
+{
+    if(0 <= hours && hours < 24 )
+       hr = hours;
+    else
+       hr = 0;
+       
+    if(0 <= minutes && minutes < 60)
+       min = minutes;
+    else
+       min = 0;
+       
+    if(0 <= seconds && seconds < 60)
+       sec = seconds;
+    else
+       sec = 0;
+}
+
+clockType::clockType()      //Default constructor
+{
+    hr = 0;
+    min = 0;
+    sec = 0;
+}
