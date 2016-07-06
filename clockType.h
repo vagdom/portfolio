482668519e7150669e8685747a8b5f0a0ec35601
vagdom <vagdom@hotmail.com>
2016-07-06 09:07:59 -0400

    Create clockType.h

---
+++ clockType.h
@@ -0,0 +1,15 @@
+//From: Data Structures Using C++
+//Author: D. S. Malik
+class clockType
+{
+public:
+  void setTime(int hours, int minutes, int seconds);
+    //Function to set the time.
+    //The time is set according to the parameters.
+    //Postcondition: hr = hours; min = minutes;
+    //               sec = seconds
+    //The function checks whether the values of hours,
+    //minutes, and seconds are valid. If a value is
+    //invalid, the default value 0 is assigned.
+    };
+  
