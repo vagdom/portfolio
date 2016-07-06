a846b97a8f931392ced28c6f1d1188c5aafee87a
vagdom <vagdom@hotmail.com>
2016-07-06 14:21:59 -0400

    Update dateType.cpp

--- dateType.cpp
+++ dateType.cpp
@@ -10,4 +10,21 @@ dateType::setDate(int month, int day, int year)
     dMonth = month;
     dDay = day;
     dYear = year;
-}    
+}  
+
+void dateType::getDate(int& month, int& day, int& year)
+{
+    month = dMonth;
+    day = dDay;
+    year = dYear;
+}
+
+void dateType::printDate()
+{
+    cout << dMonth << "-" << dDay << "-" << dYear;
+}
+
+dateType::dateType(int month, int day, int year)
+{
+    setDate(month, day, year);
+}
