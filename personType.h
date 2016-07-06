00a231fa98a456fbaa358ad7b13e3ce0c9fecd36
vagdom <vagdom@hotmail.com>
2016-07-06 12:42:46 -0400

    Create personType.h

---
+++ personType.h
@@ -0,0 +1,16 @@
+//From: Data Structures Using C++
+//Author: D. S. Malik
+class personType
+{
+public:
+  void print() const;
+  //Function to output the first name and last name
+  //in the form firstName lastname.
+  
+  void setName(string first, string last);
+  //Function to set firstName and lastName according
+  //to the parameters.
+  //Postcondition: first = firstName; last = lastName
+  
+  
+};
