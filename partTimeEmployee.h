94e9d2efb899c9fd3550e9c77ea2172292b97650
vagdom <vagdom@hotmail.com>
2016-07-06 13:27:31 -0400

    Update partTimeEmployee.h

--- partTimeEmployee.h
+++ partTimeEmployee.h
@@ -1,7 +1,7 @@
 //From: Data Structures Using C++
 //Author: D. S. Malik
 
-class aprtTimeEmployee: personType
+class partTimeEmployee: personType
 {
   public:
       void print;
@@ -9,5 +9,31 @@ class aprtTimeEmployee: personType
       //and the wages.
       //Postcondition: Outputs:
       //               firstName lastname wages are $$$$.$$
+  
+      double calculatePay();
+      //Function to calculate and return the wages.
+      //Postcondition: The pay is calculated and returned.
       
+      void setNameRateHours(string first, string last,
+                            double rate, double hours);
+      //Function to set first name, last name,
+      //pay rate, and hours worked according to the
+      //parameters.
+      //Postcondition: firstName = first; lastName = last;
+      //             payRate = rate; hoursWorked = hours
+      
+      partTimeEmployee(string first = "", string last = "",
+                       double rate = 0, double hours = 0)
+      //Constructor with parameters
+      //Sets the first name, last name, pay rate, and
+      //hours worked according to the parameters. If 
+      //no value is specified, the default values are
+      //assumed.
+      //Post condition: firstName = name; 
+      //                lastName = last; payRate = rate;
+      //                hoursWorked = hours
+      
+  private:
+      double payRate;       //stores teh pay rate
+      double hoursWorked    //stores the hours worked
 };      
