fea46da73771764be7e3f788f22ea5f9f7015289
vagdom <vagdom@hotmail.com>
2016-07-06 14:19:20 -0400

    Create dateType.cpp

---
+++ dateType.cpp
@@ -0,0 +1,13 @@
+//From: Data Structures Using C++
+//Author: D. D. Malik
+
+#include <iostream>
+
+using namespace std;
+
+dateType::setDate(int month, int day, int year)
+{
+    dMonth = month;
+    dDay = day;
+    dYear = year;
+}    
