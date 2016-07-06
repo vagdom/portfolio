b81c7eb868d85ec7c3182d388614fd62d2fb05fd
vagdom <vagdom@hotmail.com>
2016-07-06 13:44:56 -0400

    Update partTimeEmployee.cpp

--- partTimeEmployee.cpp
+++ partTimeEmployee.cpp
@@ -5,3 +5,33 @@
 #include "partTimeEmployee.h"
 
 using namespace std;
+
+void parTimeEmployee::print()
+{
+    personType::print();    //print the name of the
+                            //employee
+    cout << " wages are: " << calculatePay() << endl;                        
+}
+
+void parTimeEmployee::calculatePay()
+{
+    return (payRate * hoursWorked); 
+}   
+
+void parTimeEmployee::setNameRateHours(string first, string last,
+                                       double rate, double hours)
+{
+    personType::setName(first, last);
+    payRate = rate;
+    hoursWorked = hours;
+}  
+
+parTimeEmployee::partTimeEmployee(string first, string last,
+                                  double rate, double hours)
+                                  : personType(first, last);    //personType constructor
+{
+    
+    payRate = rate;
+    hoursWorked = hours;
+}                                  
+
