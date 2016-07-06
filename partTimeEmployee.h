1c0794a27dabb741c4779a3abdd66e9d63518e9d
vagdom <vagdom@hotmail.com>
2016-07-06 13:18:58 -0400

    Create partTimeEmployee.h

---
+++ partTimeEmployee.h
@@ -0,0 +1,13 @@
+//From: Data Structures Using C++
+//Author: D. S. Malik
+
+class aprtTimeEmployee: personType
+{
+  public:
+      void print;
+      //Function to output the first name, last name,
+      //and the wages.
+      //Postcondition: Outputs:
+      //               firstName lastname wages are $$$$.$$
+      
+};      
