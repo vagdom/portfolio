dbaf9ae053b4f7c401cbef32edea4db45712599e
vagdom <vagdom@hotmail.com>
2016-07-07 00:07:08 -0400

    Update personalInfoType.h

--- personalInfoType.h
+++ personalInfoType.h
@@ -3,6 +3,30 @@
 class personalInfoType
 {
   public:
+      void setPersonalInfo(string first, string last,
+                           int month, int day,
+                           int year, int ID);
+          //Function to set the personal information.
+          //Data members are set according to the 
+          //parameters.
+          //Postcondition: firstName = first; 
+          //               lastName = last;
+          //               dDay = day; dMonth = month;
+          //               dYear = year; personID = ID
+      
+      void printPersonalInfo() const;
+          //Function to print the personal information.
+          
+      personalInfoType(string first = "", string last = "",
+                       int month = 1, int day = 1,
+                       int year = 1900, int ID = 0);
+          //Constructor
+          //Data members are set according to the 
+          //parameters.
+          //Postcondition: firstName = first; 
+          //               lastName = last;
+          //               dDay = day; dMonth = month;
+          //               dYear = year; personID = ID 
   private:
 };  
   
