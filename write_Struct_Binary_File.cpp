d3e3ed08b56e0f162f6b8b2410f1b5fd4d0df20c
vagdom <vagdom@hotmail.com>
2016-07-14 11:19:35 -0400

    Create write_Struct_Binary_File.cpp

---
+++ write_Struct_Binary_File.cpp
@@ -0,0 +1,10 @@
+//This program uses a structure variable to store a record to a file.
+//From: Starting Out with C++
+//Author: Tony Gaddis
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//Decla
