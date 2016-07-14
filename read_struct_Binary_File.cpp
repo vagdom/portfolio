7cb3035b7472d9ed8d78449fb37984a582380a6c
vagdom <vagdom@hotmail.com>
2016-07-14 12:15:52 -0400

    Update read_struct_Binary_File.cpp

--- read_struct_Binary_File.cpp
+++ read_struct_Binary_File.cpp
@@ -31,37 +31,45 @@ int main()
   //Open the file for input in binary mode
   people.open("people.dat", ios::in | ios::bianary);
   
-  do
+  //Test for errors
+  if (!people)
   {
-    
-      //Get data about a person
-      cout << "Enter the following data about a "
-           << "person:\n";
-      cout << "Name: ";
-      cin.getline(eprson.name, NAME_SIZE);
-      cout << "Age: ";
-      cin >> peson.age;
-      cin.ignore(); //Skip over the remaining newline
-      cout << "Address line 1: ";
-      cin.getline(person.address1, ADDR_SIZE);
-      cout << "Address line 2: ";
-      cin.getline(person.address2, ADDR_SIZE);
-      cout << "Phone: ";
-      cin.getline(person.phone, PHONE_SIZE);
-      
-      //Write the contents of the person structure to the file
-      people.write(reinterpret_cast,char *>(&person), sizeof(person));
-      
-      //Determine whether the user wants to write another record
-      cout << "Do you want to enter another record? ";
-      cin >> again;
-      cin.ignore(); //Skip over the remaining newline
-      
-  } while ( again == 'Y' || again == 'y');
+            cout << "Error opening file. Program aborting.\n ";
+            return 0;
+  }
   
-  //Close the file
+  cout << "Here are the people in the file:\n\n";
+  //Read the first record from the file
+  people.read(reinterpret_cast,char *>(&person), sizeof(person));
+  
+  //While not a the end of file, display
+  //the records
+  while (!people.eof))
+  {
+            //Display the record
+            cout << "Name: ";
+            cout << person.name << endl;
+            cout << "Age: ";
+            cout << person.age << endl;
+            cout << "Address line 1: ";
+            cout << person.address1 << endl;
+            cout << "Address line 2: ";
+            cout << person.address2 << endl;
+            cout << "Phone: ";
+            cout << person.phone << endl;
+            
+            //Wait for the user to press the Enter key
+            cout << "\nPress the Enter key to see the next record.\n";
+            cin.get(again);
+            
+            //Read the next record from the file
+            people.read(reinterpret_cast,char *>(&person), sizeof(person));
+  }
+  
+  cout << "That's all the data in the file!\n";
+  
   people.close();
   
   return 0;
-  
+ 
 }
