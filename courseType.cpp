c81dfb9d4e4a560f0ae56b88b46fc626cbe1225f
vagdom <vagdom@hotmail.com>
2017-11-09 21:51:21 -0500

    Create courseType.cpp

---
+++ courseType.cpp
@@ -0,0 +1,9 @@
+void couseType::setCourseInfo(string cName, string cNO,
+                              char grade, int credits)
+{
+      courseName = cName;
+      courseNo = cNo;
+      courseGrade = grade;
+      
+}
+                              
