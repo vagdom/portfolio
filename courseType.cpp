da7d4bb31fb06d07592b0fb233127cd7ffecd042
vagdom <vagdom@hotmail.com>
2017-11-11 10:10:18 -0500

    Update courseType.cpp

--- courseType.cpp
+++ courseType.cpp
@@ -1,9 +1,87 @@
 void couseType::setCourseInfo(string cName, string cNO,
                               char grade, int credits)
 {
-      courseName = cName;
-      courseNo = cNo;
-      courseGrade = grade;
-      
+  courseName = cName;
+  courseNo = cNo;
+  courseGrade = grade;
+  courseCredits = credits;  
 }
-                              
+
+void courseType::print(bool isGrade)
+{
+  
+  cout << left;
+  cout << setw(08) << courseNo << "   ";
+  cout << setw(15) << courseName;
+  cout.unsetf(ios::left);
+  cout << setw(03) << courseCredits << "       ";
+  
+  if(isGrade)
+    cout << setw(04) << courseGrade << endl;
+  else
+    cout << setw(04) << "***" << endl;
+}
+
+void courseType::print(ofstream& outp, bool isGrade)
+{
+  outp << left;
+  outp << setw(08) << courseNo << "   ";
+  outp << setw(15) << courseName;
+  outp.unsetf(ios::left);
+  outp << setw(03) << courseCredits << "   ";
+  
+  if(isGrade)
+    outp << setw(04) << courseGrade << endl;
+  else
+    outp << setw(04) << "***" << endl;
+}
+
+courseType::courseType(string cName, string cNo,
+                       char grade, int credits)
+{
+  setCourseInfo(cName, cNo, grade, credits);
+}
+
+char courseType::getGrade()
+{
+  return courseGrade;
+}
+
+void courseType::getCourseNumber(string& cNo)
+{
+  cNo = courseNo;
+}
+
+bool courseType::operator ==(const courseType& right) const
+{
+  return courseNo == right.courseNo);
+}
+
+bool courseType::operator !=(const courseType& right) const
+{
+  return courseNo != right.courseNo);
+}
+
+bool courseType::operator <=(const courseType& right) const
+{
+  return courseNo <= right.courseNo);
+}
+
+bool courseType::operator <(const courseType& right) const
+{
+  return courseNo < right.courseNo);
+}
+
+bool courseType::operator >=(const courseType& right) const
+{
+  return courseNo >= right.courseNo);
+}
+
+bool courseType::operator >(const courseType& right) const
+{
+  return courseNo > right.courseNo);
+}
+
+
+
+
