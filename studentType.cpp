3da9775c162622d06e374b5a24f9efb84cff8519
vagdom <vagdom@hotmail.com>
2017-11-11 12:47:03 -0500

    Update studentType.cpp

--- studentType.cpp
+++ studentType.cpp
@@ -1,1 +1,14 @@
-void
+void studentType::setInfo(string fName, string lNmae, int ID,
+                          bool isTPaid,
+                          vector<courseType> courses)
+{
+  personType(fName, lName);        //set the name
+  
+  sId = ID;
+  isTuitionPaid = isTPaid;
+  numberOfCourses = courses.soze();
+  
+  coursesEnrolled = courses;
+  
+  //sort
+    
