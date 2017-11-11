1b230b8deada460d58d0d119a5dd7cbb02c78d7f
vagdom <vagdom@hotmail.com>
2017-11-11 14:30:13 -0500

    Update gradeReport.cpp

--- gradeReport.cpp
+++ gradeReport.cpp
@@ -5,12 +5,47 @@
 #include <vector>
 #include <iterator>
 
+//Function prototypes
+void getStudentData(ifstream& infile, vector<studentType> &studentList);
+
+void printGradeReports(ofstream& outfile, vector<studentType> studentList, double tuitionRate);
+
 using namespace std;
 
 int main()
 {
 
-
+  vector<studentType> studentList;
+  
+  double tuitionRate;
+  
+  ifstream infile;
+  ofstream outfile;
+  
+  infile.open("inputFile.txt");
+  
+  if(!infile)
+  {
+    cerr << "Input file does not exist. " << endl;
+    cerr << "Program Aborted. " << endl;
+    return 1;
+  }
+  
+  outfile.open("outputFile.txt");
+  
+  infile >> tuitionRate;
+  
+  getStudentData(infile, studentList);
+  
+  printGradeReports(outfile, studentList, tuitionRate);
+  
+  infile.close();
+  outfile.close();
+  
+  return 0;
+  
+}
+  
 
 
 
